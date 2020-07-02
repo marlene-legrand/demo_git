96471b890ea5ea8c1aab34d3b881d68fab6d8204
marlene legrand <marlene.legrand8@gmail.com>
2020-07-02 11:50:40 +0200

    ajout des getters/setters à la classe Utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -23,6 +23,31 @@ public class Utilisateur {
 	}
 	
 	
+	/*__________ getters/setters ___________*/
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
 	
 	
 }//end class
