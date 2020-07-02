3caaf161c5ce897746ed87cfac417b887bacdc3e
marlene legrand <marlene.legrand8@gmail.com>
2020-07-02 11:34:45 +0200

    ajout des constructeurs a la classe utilisateur

--- src/Utilisateur.java
+++ src/Utilisateur.java
@@ -10,6 +10,19 @@ public class Utilisateur {
 	private String nom;
 	private String prenom;
 	
+	/*__________ ctor ___________*/
+	public Utilisateur() {
+		
+	}
+
+	public Utilisateur(int id, String nom, String prenom) {
+		super();
+		this.id = id;
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	
+	
 	
 	
 }//end class
